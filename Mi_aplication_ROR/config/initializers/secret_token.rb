7ff34dc7f50f6ff95370efb3815d6af38b430dc4
YulaySC <yulyssc@gmail.com>
2014-11-18 00:51:55 -0600

    ROR con user.rb

---
+++ Mi_aplication_ROR/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TP3::Application.config.secret_key_base = 'a0c939d0fb90c3cf5d28a36da247661935f928ac4c8a2151c5a2ac3eed4c1dadef046530394ed20c7571db58de0925c0024af4dfe077be1f3e988388f4e0c320'
