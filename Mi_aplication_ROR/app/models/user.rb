6d221a7161f4a75aec7aa6f99c5716bb646f3075
SussanaRS <suramirez92@gmail.com>
2014-11-19 15:52:45 -0600

    Revert "ROR con user.rb"
    This reverts commit 7ff34dc7f50f6ff95370efb3815d6af38b430dc4.

--- Mi_aplication_ROR/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-## significa que la clase Base hereda de ActiveRecord
-## se define en singular pero se interpreta como plural en la BD
-class User < ActiveRecord::Base
-	validates :nombre, presence: true, length: {maximum: 10}
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with:VALID_EMAIL_REGEX }
-	validates :password, presence: true, length: {maximum: 16}
-
-end
